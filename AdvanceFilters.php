b3c7de352f1cba9ead30f525d3af0149d8807c47
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-03-25 10:48:34 +0600

    filter

---
+++ AdvanceFilters.php
@@ -0,0 +1,74 @@
+<?php
+$fonts = "arial";
+$fontscolor = "#FCBC41";
+?>
+<!DOCTYPE html>
+
+<head>
+    <title>Php coding</title>
+    <style>
+        body {
+            font-family:
+                <?php echo $fonts;
+                ?>
+            ;
+        }
+
+        .phpCode {
+            width: 100%;
+            background: #ddd;
+            margin: 0 auto;
+        }
+
+        .main {
+            min-height: 600px;
+            text-align:
+                <?php echo "center" ?>
+
+        }
+
+        .header,
+        .footer {
+            background:
+                <?php echo "#444" ?>
+            ;
+            color:
+                <?php
+                echo $fontscolor;
+                ?>
+            ;
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="phpCode">
+        <div class="header">
+            <h2>PHP fundamental training</h2>
+        </div>
+        <div class="main">
+            <?php
+            $num = 300;
+            $min = 1;
+            $max = 200;
+            if (filter_var($num, FILTER_VALIDATE_INT, array("options" => array("min_range" => $min, "max_range" => $max))) === false) {
+                echo "IT is not valid";
+            } else {
+                echo "it is valid";
+            }
+
+            ?>
+        </div>
+
+        <div class="footer">
+            <h2>
+                <?php echo "www.triningphp.com" ?>
+            </h2>
+        </div>
+    </section>
+
+</body>
+
+</html>
---
+++ filters.php
@@ -0,0 +1,76 @@
+<?php
+$fonts = "arial";
+$fontscolor = "#FCBC41";
+?>
+<!DOCTYPE html>
+
+<head>
+    <title>Php coding</title>
+    <style>
+        body {
+            font-family:
+                <?php echo $fonts;
+                ?>
+            ;
+        }
+
+        .phpCode {
+            width: 100%;
+            background: #ddd;
+            margin: 0 auto;
+        }
+
+        .main {
+            min-height: 600px;
+            text-align:
+                <?php echo "center" ?>
+
+        }
+
+        .header,
+        .footer {
+            background:
+                <?php echo "#444" ?>
+            ;
+            color:
+                <?php
+                echo $fontscolor;
+                ?>
+            ;
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="phpCode">
+        <div class="header">
+            <h2>PHP fundamental training</h2>
+        </div>
+        <div class="main">
+            <?php
+            $str = "<h2>I am learning php<h2>";
+            $newstr = filter_var($str, FILTER_SANITIZE_STRING);
+            echo $newstr . "<br/>";
+
+            $int = 30.6;
+            if (filter_var($int, FILTER_VALIDATE_INT)) {
+                echo "It is integer value";
+            } else {
+                echo "It is not Integer value";
+            }
+
+            ?>
+        </div>
+
+        <div class="footer">
+            <h2>
+                <?php echo "www.triningphp.com" ?>
+            </h2>
+        </div>
+    </section>
+
+</body>
+
+</html>
